9fecb0f272e390ae185a704e40fedad8ca4f9657
Thiago Arthur <thlimabsgim@gmail.com>
2022-12-12 11:47:56 -0400

    Implementada a busca por titulo do filme.

--- src/app/pages/filmes/filmes.page.ts
+++ src/app/pages/filmes/filmes.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
-import { FilmeService } from 'src/app/services/filme.service';
+import {ApiResult, FilmeService, IFilmeApi} from 'src/app/services/filme.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,21 +12,41 @@ export class FilmesPage implements OnInit {
   filmes : any [] = [];
   currentPage = 1;
   imageBaseUrl = environment.images
+  listarFilmes: ApiResult;
 
   constructor(private filmeService: FilmeService, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
     this.loadFilmes()
-    
   }
 
-  async loadFilmes(event?: InfiniteScrollCustomEvent){  
+  defaultTermo(event : any){
+    const busca = event.target.value;
+    if (busca == 0){
+      this.loadFilmes();
+    }
+  }
+
+  buscarFilmes(event: any) {
+    console.log(event.target.value);
+    const busca = event.target.value;
+    if (busca && busca.length > 0) {
+      this.filmeService.buscarFilmes(busca).subscribe(dados => {
+          console.log(dados);
+          this.listarFilmes = dados;
+        }
+      );
+    } else {
+      this.defaultTermo(event);
+    }
+  }
+  async loadFilmes(event?: InfiniteScrollCustomEvent){
     const loading = await this.loadingCtrl.create({
       message: 'Carregando...',
       spinner: 'bubbles',
     })
     await loading.present();
-    
+
     this.filmeService.getFilmesMaisVotados(this.currentPage).subscribe(res => {
       loading.dismiss();
       this.filmes.push(...res.results);
@@ -44,4 +64,7 @@ export class FilmesPage implements OnInit {
     this.loadFilmes(event);
   }
 
+  exibirFilme(filme: IFilmeApi) {
+    this.filmeService.guardarDados('filme', filme);
+  }
 }
--- src/app/services/filme.service.ts
+++ src/app/services/filme.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import {map, Observable} from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+export interface IFilmeApi {
+  poster_path?: string;
+  adult?: boolean;
+  overview?: string;
+  release_date?: string;
+  genre_ids?: number[];
+  id?: number;
+  original_title?: string;
+  original_language?: string;
+  title?: string;
+  backdrop_path?: string;
+  popularity?: number;
+  vote_count?: number;
+  video?: boolean;
+  vote_average?: number;
+}
 
 export interface ApiResult{
   page: number;
-  results: any[];
+  results: IFilmeApi[];
   total_pages: number;
   total_results: number;
 }
@@ -15,9 +31,29 @@ export interface ApiResult{
   providedIn: 'root'
 })
 export class FilmeService {
-
+  private dados: any = [];
   constructor(private http: HttpClient) { }
 
+  guardarDados(index: string, dados: any): boolean {
+    if (index) {
+      this.dados[index] = dados;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  pegarDados(index: string): any {
+    if (index) {
+      return this.dados[index];
+    } else {
+      return null;
+    }
+  }
+
+  deletarDados(index: string): boolean {
+    return delete this.dados[index];
+  }
   getFilmesMaisVotados(page = 1): Observable<ApiResult>{
     return this.http.get<ApiResult>
     (`${environment.baseUrl}/movie/popular?page=${page}&api_key=${environment.apiKey}&language=pt-BR`);
@@ -26,5 +62,13 @@ export class FilmeService {
   getDetalheFilme(id: string){
     return this.http.get(`${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}&language=pt-BR`
     );
+  }
+
+  buscarFilmes(busca : string):Observable<ApiResult>{
+    const url = `${environment.baseUrl}/search/movie?api_key=${environment.apiKey}&language=pt-BR&query=${busca}`;
+
+    return this.http.get<ApiResult>(url).pipe(
+      map(retorno => retorno),
+    )
   }
 }
